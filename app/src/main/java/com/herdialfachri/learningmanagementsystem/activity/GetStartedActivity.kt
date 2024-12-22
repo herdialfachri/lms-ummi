7109ca313b5a92dd6f74d6165c8794ba60281694
herdialfachri <alfachri75@gmail.com>
2024-12-22 22:10:04 +0700

    first commit

---
+++ app/src/main/java/com/herdialfachri/learningmanagementsystem/activity/GetStartedActivity.kt
@@ -0,0 +1,25 @@
+package com.herdialfachri.learningmanagementsystem.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.herdialfachri.learningmanagementsystem.R
+
+class GetStartedActivity : AppCompatActivity() {
+    private lateinit var startButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_started)
+
+        startButton = findViewById(R.id.startButton)
+
+        startButton.setOnClickListener {
+            val intent = Intent(this@GetStartedActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
